4bd14a8451ea69b11303635556e133ac7f4d61c9
AlbericC <alberic.atama@gmail.com>
2016-02-16 16:29:29 +0100

    getxkcd with futures

--- getxkcd.py
+++ getxkcd.py
@@ -1,13 +1,21 @@
 #! /usr/bin/env python3.5
+
 from concurrent.futures import ThreadPoolExecutor
-import begin
 from path import Path
-import xkcd
 from time import sleep
 
+import begin
+import xkcd
 
-def fetch(number, destination):
-    """fetches a missing XKCD comic
+from tqdm import tqdm
+
+
+DEFAULT_PATH = str(Path("~").expand() / "xkcd")
+
+
+def fetch(number, destination, tries=3):
+    """
+    fetches a missing XKCD comic
 
     Args:
         number: the number id of the comic
@@ -15,45 +23,59 @@ def fetch(number, destination):
     Returns:
         None
     """
-    retry = 3
-    while retry:
-        try:
-            comic = xkcd.Comic(number)
-            image_name = comic.imageName
-        except HTTPError as e:
-            if e.code != 404:
-                print(e)
-                retry -= 1
-                sleep(0.5)
-            else:
-                return 'Missing: {}'.format(number)
+    comic = xkcd.Comic(number)
+    image_name = comic.imageName
 
-    # print("trying {}".format(number))
     target = "{}/{:>04}-{}".format(destination, number, image_name)
     if Path(target).isfile() and Path(target + ".txt").isfile():
-        # print("{:> 5}: ignored".format(number))
-        return 'Nothing to do for {}'.format(number)
+        return
     comic.download(outputFile=target)
     alt = comic.getAltText()
-    print("{:>5}: downloaded".format(number))
+    # print("{:>5}: downloaded".format(number))
     with open(target + ".txt", "w") as spam:
         spam.write(alt)
         # print("{:>5}: alt text written".format(number))
     return 'Got {}'.format(number)
 
 
-@begin.start
-def run(path: "Path to which the comics are to be downloaded"=
-        str(Path("~").expand() / "xkcd")):
-    """Get the full XKCD comics from the web"""
+def setup(path):
+    """Givent a path, setup the directory structure"""
     path = Path(path).expand()
     if not path.isdir():
         path.mkdir()
-    with ThreadPoolExecutor(max_workers=32) as executor:
-        futures = []
-        for comic_num in range(1, xkcd.getLatestComicNum() + 1):
-            f = executor.submit(fetch, comic_num, path)
-            futures.append(f)
-            print('future submitted')
-        for f in futures:
-            print('checking a future {}'.format(f.result(timeout=10)))
+    return
+
+
+def batch_submit(executor, items):
+    """submit items to an executor, return a list of futures"""
+    futures = []
+    for comic_num, path in items:
+        f = executor.submit(fetch, comic_num, path)
+        futures.append(f)
+    return futures
+
+
+def progress(futures, total):
+    """display a progress bar while futures are done"""
+    with tqdm(total=total) as pbar:
+        while futures:
+            sleep(0.5)
+            not_yet = []
+            for fut in futures:
+                if fut.done():
+                    pbar.update(1)
+                else:
+                    not_yet.append(fut)
+            futures = not_yet
+    return
+
+
+@begin.start
+def run(path: "Path to which the comics are to be downloaded"=DEFAULT_PATH):
+    """Get the full XKCD comics from the web"""
+    setup(path)
+    executor = ThreadPoolExecutor(max_workers=32)
+    comics = xkcd.getLatestComicNum()
+    items = [(num, path) for num in range(1, xkcd.getLatestComicNum() + 1)]
+    futures = batch_submit(executor, items)
+    progress(futures, total=comics)
